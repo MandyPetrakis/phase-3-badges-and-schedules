54da203627197e2200af2e917121108f9d015503
Mandy Petrakis <mandypetrakis@gmail.com>
2023-03-15 12:37:24 -0700

    commit tree 904a51757fb9f840449baee6c0a9d51b36ea6003

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator names 
+   array = names.map{ |name| badge_maker(name) }
+   array
+end
+
+def assign_rooms speakers
+   list =  speakers.map{ |speaker|  "Hello, #{speaker}! You'll be assigned to room #{speakers.index(speaker)+1}!"
+}
+    list
+end
+
+def printer list
+ batch_badge_creator(list).each{ |badge| puts badge}
+  assign_rooms(list).each{|room| puts room}
+end
